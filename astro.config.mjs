096a56a1900bde96ca9fd3eec41f99e34ff2893d
Boston343 <reap3r@mailfence.com>
2025-05-08 21:11:38 -0400

    chore: cleanup and formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
-import keystatic from "@keystatic/astro";
 import netlify from "@astrojs/netlify";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import keystatic from "@keystatic/astro";
 import compress from "@playform/compress";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
